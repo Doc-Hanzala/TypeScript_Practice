0947ef53af68b081ee32a4dc68d488aa9f23ee97
hanzala tanveer <810hanzalatanveer@gmail.com>
2024-04-23 18:25:12 -0700

    worked with type annotations

---
+++ src/tutorial/1-type-annotations.ts
@@ -0,0 +1,14 @@
+// string
+let firstName: string = "hanzala";
+firstName = firstName.toUpperCase();
+console.log(firstName);
+
+// number
+let age: number = 24;
+age = age - 21;
+console.log(age);
+
+// boolean
+let isAdult:boolean=age >=18
+console.log(isAdult);
+
