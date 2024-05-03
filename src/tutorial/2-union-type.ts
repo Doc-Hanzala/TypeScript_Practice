b67e4b35b606b646a4cbe48d711c2c1d8efb3ac3
hanzala tanveer <810hanzalatanveer@gmail.com>
2024-05-02 18:34:34 -0700

    worked with "any type" and an associated challenge

--- src/tutorial/2-union-type.ts
+++ src/tutorial/2-union-type.ts
@@ -1,18 +1,33 @@
-let tax: number | string = 100;
+// <----- Union type ------>
 
-tax='100'
+let tax: number | string = 100;
+tax = "100";
 console.log(tax);
 
 // ----------------------
-let reqStatus : 'pending'|'error'|'success' = 'pending'
-reqStatus="success"
+let reqStatus: "pending" | "error" | "success" = "pending";
+reqStatus = "success";
 console.log(reqStatus);
 
 // ---------------
-
-let houseNo :23|34|45= 45
-houseNo=23
+let houseNo: 23 | 34 | 45 = 45;
+houseNo = 23;
 console.log(houseNo);
 
+// <-------------- Any type ----------->
+let lastName: any = "hanzala";
+lastName = 234;
+console.log(lastName);
 
+// challenge 1
+let orderStatus: "processing" | "shipped" | "delivered" = "processing";
+orderStatus = "shipped";
+orderStatus = "delivered";
 
+console.log(orderStatus);
+
+// challenge 2
+let discount:number |string=20
+discount='20%'
+
+console.log(discount);
