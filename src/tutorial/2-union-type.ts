d301a5112d0677c662550e78ebe449c0e596318e
hanzala tanveer <810hanzalatanveer@gmail.com>
2024-05-02 18:25:04 -0700

    worked with union type

---
+++ src/tutorial/2-union-type.ts
@@ -0,0 +1,18 @@
+let tax: number | string = 100;
+
+tax='100'
+console.log(tax);
+
+// ----------------------
+let reqStatus : 'pending'|'error'|'success' = 'pending'
+reqStatus="success"
+console.log(reqStatus);
+
+// ---------------
+
+let houseNo :23|34|45= 45
+houseNo=23
+console.log(houseNo);
+
+
+
